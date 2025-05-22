7d481671e53e53753347dbb60dc5b0bf787f7645
rajmendra <rajcool.lko@gmail.com>
2025-05-22 23:58:44 +0530

    added new function

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function helloWorld() {
     console.log("check this code");
     console.log("check this one as well");
   }
+
+  function getApiData() {
+    fetch('https://api.example.com/data')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+
+    console.log('Fetching complete');
+}
